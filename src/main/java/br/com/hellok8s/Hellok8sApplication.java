230bc09b23b6db84cad3edeed593158b00257f65
rafael.fernandes <rafael.fernandes@host.docker.internal>
2020-08-31 01:25:29 -0300

    Commit Inicial

---
+++ src/main/java/br/com/hellok8s/Hellok8sApplication.java
@@ -0,0 +1,13 @@
+package br.com.hellok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellok8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellok8sApplication.class, args);
+	}
+
+}
